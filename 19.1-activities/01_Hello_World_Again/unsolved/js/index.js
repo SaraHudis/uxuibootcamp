88cf381c9bdbfaf69efe0c37315bbcd18329c81c
SaraHudis <104800920+SaraHudis@users.noreply.github.com>
2022-10-18 21:29:03 -0500

    19.1_after class

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,5 @@
+// Create a variable below
+
+// Console.log() your variable here.
+var helloWorld = 'Hello World!'; 
+console.log(helloWorld)
---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,26 @@
+// Create the addNumbers function here.
+
+// Create the stringConcat function here.
+var helloWorld = 'Hello World!'; 
+console.log(helloWorld)
+
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+
+    var five = two + three;
+
+    console.log(five);    
+}
+
+addNumbers();
+
+function stringConcat() {
+var taco = 'taco';
+var cat = 'cat';
+
+var tacoCat = 'taco' + ' ' + 'cat';
+
+alert('tacocat');
+
+stringConcat()
