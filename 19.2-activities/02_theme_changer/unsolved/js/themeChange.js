536dae67e079de3a25e1725919c8886f47bf5c7f
SaraHudis <104800920+SaraHudis@users.noreply.github.com>
2022-10-20 21:46:59 -0500

    10.20_edits

---
+++ 19.2-activities/02_theme_changer/unsolved/js/themeChange.js
@@ -0,0 +1,12 @@
+$("themeChange").on("click", function(){
+    if ($(this).hasClass("regular")) {
+        $("#theme").attr("href", "css/blank.css")
+    }
+    else if (condition)) {
+        $("#theme").attr("href", "css/blue.css")
+    }
+    else if (condition)) {
+        $("#theme").attr("href", "css/dark.css")
+    }
+});
+  
