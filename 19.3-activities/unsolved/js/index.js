bdc1f4df4563d5d7673de6c9bedf7157b09d50d5
SaraHudis <104800920+SaraHudis@users.noreply.github.com>
2022-10-24 22:41:19 -0500

    10.24

--- 19.3-activities/unsolved/js/index.js
+++ 19.3-activities/unsolved/js/index.js
@@ -2,3 +2,18 @@ $(".toggle").click(function(){
     $(this).find(".toggleContainer").toggleClass("active");
     $(this).siblings(".toggle").find(".togglecontainer").removeClass("active");
 });
+
+$(".searchbutton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")) {
+        $(".searchBar").css("height", "100vh");
+        $("#searchForm").css("opacity","1");
+        $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    }
+    else {
+        $(".searchBar").css('height', "0vh");
+        $("#searchForm").css("opacity","0");
+        $(".searchIcon").removeClass("fa-times").addClass("fa-search")
+    }
+  });
+  
